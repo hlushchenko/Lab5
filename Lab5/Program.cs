2c928932bad09a0faff469e2b006ffb49a85c231
Aleksey <alekseyhonchar@gmail.com>
2021-05-08 19:56:04 +0300

    IsNumber() added

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections;
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace Lab5
 {
@@ -8,23 +8,9 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            Hashtable a = new Hashtable();
-            var sumNode = new OperatorNode(OperatorNode.OperationType.Sum, null);
-            var multNode = new OperatorNode(OperatorNode.OperationType.Multiplication, null);
-            var assignNode = new OperatorNode(OperatorNode.OperationType.Assign, null);
-            var AssVar = new VariableNode("oleg", null, a);
-            var const12 = new VariableNode("oleg", null, a);
-            var const13 = new VariableNode("oleg", null, a);
-            var const5 = new ConstantNode(5, null);
-            
-            assignNode.AddLeftOperand(AssVar);
-            assignNode.AddRightOperand(new ConstantNode(10, null));
-            var b = assignNode.Value;
-            multNode.AddLeftOperand(const13);
-            multNode.AddRightOperand(const5);
-            sumNode.AddLeftOperand(const12);
-            sumNode.AddRightOperand(multNode);
-            Console.WriteLine(sumNode.Value);
+            string task = "5+2*3";
+            Stack<char> poland = Utilities.Parse(task);
+            Console.ReadKey();
         }
     }
 }
---
+++ Lab5/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Utilities
+    {
+        public static Stack<char> Parse(string task)
+        {
+            Stack<char> tempStack = new Stack<char>();
+            Stack<char> resultStack = new Stack<char>();
+            for (int i = 0; i < task.Length; i++)
+            {
+
+            }
+            return null;
+        }
+
+        private static bool IsNumber(char a) => a >= '0' && a <= '9';
+    }
+}
