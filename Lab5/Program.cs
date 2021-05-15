825d2f26443ae295580b9e0ecc7c87abcea87104
Hluushchenko <Hluushchenko@gmail.com>
2021-05-15 09:01:32 +0300

    Final version

--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            CodeFileReader codeFile = new CodeFileReader("D:/Code.txt");
+            CodeFileReader codeFile = new CodeFileReader(args[0]);
             SyntaxTree syntaxTree = new SyntaxTree();
             syntaxTree.AddFromStringArray(codeFile.Read());
             syntaxTree.PrintResult();
