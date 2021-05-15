f32e1eeee5ec6e5d64ee1f2550c73718a4bebeed
Hluushchenko <Hluushchenko@gmail.com>
2021-05-15 10:14:29 +0300

    ConditionalNode Added

---
+++ Lab5/Nodes/ConditionalNode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab5
+{
+    public class ConditionalNode : Node
+    {
+        
+        public override NodeType GetNodeType() => NodeType.Conditional;
+        private Node[] _children;
+
+        public ConditionalNode(Node parent)
+        {
+            Parent = parent;
+            _children = new Node[3];
+        }
+
+        public override void AddChild(Node node)
+        {
+            if (IsFull)
+            {
+                int current = 0;
+                while (_children[current] != null)
+                {
+                    current++;
+                }
+                _children[current] = node;
+                if (current == 2)
+                {
+                    IsFull = true;
+                }
+            }
+        }
+        
+        public override double Value {
+            get
+            {
+                if (_children[0].Value > 0) return _children[1].Value;
+                return _children[2].Value;
+            }
+            set => throw new Exception();
+        }
+    }
+}
--- Lab5/Nodes/NodeType.cs
+++ Lab5/Nodes/NodeType.cs
@@ -7,6 +7,7 @@
         Operator,
         Constant,
         Root,
-        Assign
+        Assign, 
+        Conditional
     }
 }
